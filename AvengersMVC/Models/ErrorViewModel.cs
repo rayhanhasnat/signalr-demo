1144f2f181a13b43795e722beb155b670acc7b60
Rayhan Hasnat <r.hasnat@gmail.com>
2021-09-25 19:03:47 +0600

    Initial commit

---
+++ AvengersMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AvengersMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
